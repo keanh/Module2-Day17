e1e9d5a5fcd67f01b8982c19709ea7f707b05f9c
keanh <keanh275@gmail.com>
2020-03-08 22:00:14 +0700

    fix2

--- QuanLySanPham/src/Test.java
+++ QuanLySanPham/src/Test.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Scanner scanner = new Scanner(System.in);
+        File file = new File("D:\\Module 2\\Day 17\\QuanLySanPham\\ProductList");
+        OutputStream outputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream
+                ("D:\\Module 2\\Day 17\\QuanLySanPham\\ProductList"));
         int number = -1;
         while (number != 0) {
             menu();
             number = scanner.nextInt();
-            File file = new File("D:\\Module 2\\Day 17\\QuanLySanPham\\ProductList");
-            OutputStream outputStream = new FileOutputStream(file);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
             switch (number) {
                 case 1:
                     ArrayList<SanPham> list = new ArrayList<>();
@@ -41,10 +43,7 @@ public class Test {
                     objectOutputStream.flush();
                     break;
                 case 2:
-                    ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream
-                            ("D:\\Module 2\\Day 17\\QuanLySanPham\\ProductList"));
                     SanPham sanPhamRead = (SanPham) objectInputStream.readObject();
-                    objectInputStream.close();
                     System.out.println("ID: " + sanPhamRead.getID());
                     System.out.println("Tên sản phẩm: " + sanPhamRead.getName());
                     System.out.println("Tên hãng sản phẩm: " + sanPhamRead.getBrandName());
@@ -53,6 +52,7 @@ public class Test {
                 case 4:
                     System.exit(4);
                     objectOutputStream.close();
+                    objectInputStream.close();
             }
         }
     }
